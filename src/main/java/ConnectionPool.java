2950135aad428836b5429c822fb998e3a6b3e2c5
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-05-18 15:47:48 -0500

    Make singleton thread safe

--- src/main/java/ConnectionPool.java
+++ src/main/java/ConnectionPool.java
@@ -3,20 +3,23 @@ public class ConnectionPool {
 
     private Connection connection = new Connection();
 
-    private ConnectionPool(){
-        if(pool != null){
+    private ConnectionPool() {
+        if (pool != null) {
             throw new RuntimeException("Use getInstance() method to get the single instance of this class");
         }
     }
 
-    public static ConnectionPool getPool(){
-        if(pool == null){
-            pool = new ConnectionPool();
+    public static ConnectionPool getPool() {
+        if (pool == null) {
+            synchronized (ConnectionPool.class) {
+                if (pool == null) pool = new ConnectionPool();
+            }
+
         }
         return pool;
     }
 
-    public Connection getConnection(){
+    public Connection getConnection() {
         return connection;
     }
 }
